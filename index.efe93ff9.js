2666323a5aebfab723c2b8854c6581f8c3b69da6
Mykola <mykola.stanj@gmail.com>
2024-11-11 22:19:35 +0200

    make build

---
+++ index.efe93ff9.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table"),t=document.querySelector("thead"),r=document.querySelector("tfoot"),o=e.querySelectorAll("tbody tr"),l=t.querySelectorAll("th"),c=l[1].cloneNode(!0);l[3].after(c);var d=r.querySelectorAll("th"),u=d[1].cloneNode(!0);d[3].after(u),o.forEach(function(e){var t=e.querySelector("td:nth-child(2)").cloneNode(!0);e.querySelector("td:nth-child(4)").after(t)});
+//# sourceMappingURL=index.efe93ff9.js.map
